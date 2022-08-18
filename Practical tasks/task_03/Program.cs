334866fa949a842d0c9ddb6097e205f25649ef5b
ilnar <ilnar-sov@mail.ru>
2022-08-19 01:26:40 +0300

    Практические задания к семинару 4 по C#

--- Practical tasks/task_03/Program.cs
+++ Practical tasks/task_03/Program.cs
@@ -1,5 +1,35 @@
 ﻿// Написать программу копирования массива
-Console.Clear();
 
+void FillArray(int[] array, int min, int max)
+{
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++) array[i] = random.Next(min, max);
+}
+int NewArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    int result = 0;
+    for (int i = 0; i < array.Length; i++) 
+    {
+        newArray[i] = array[i]*2;
+        result = result + newArray[i];
+    }
+    return result;
+}
+void PrintArray(int[] array)
+{
+        foreach (var element in array) Console.Write($"{element} "); 
+        Console.WriteLine();
+}
+
+Console.Clear();
+int[] array = new int[10];
+FillArray(array, -100, 100);
+PrintArray(array);
+
+//int[] newArray = 
+
+//NewArray(array);
+//PrintArray(array);
 
 
