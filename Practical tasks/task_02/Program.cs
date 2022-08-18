b2bf167de5269d84fb5a5f00eec3aafe650253b7
ilnar <ilnar-sov@mail.ru>
2022-08-19 01:25:37 +0300

    Практические задания к семинару 4 по C#

--- Practical tasks/task_02/Program.cs
+++ Practical tasks/task_02/Program.cs
@@ -1,6 +1,50 @@
 ﻿// Написать программу масштабирования фигуры
-// Тут для тех кто далеко улетел, чтобы задавались вершины фигуры списком (одной строкой)
-// например: "(0,0) (2,0) (2,2) (0,2)"
-// коэффициент масштабирования k задавался отдельно - 2 или 4 или 0.5
-// В результате показать координаты, которые получатся.
-// при k = 2 получаем "(0,0) (4,0) (4,4) (0,4)"
+
+
+double InputNumber(string text)
+{
+    Console.Write(text);
+    double number = Double.Parse(Console.ReadLine());
+    return number;
+}
+void InputCoord(double[] array, string text)
+{
+    int count = 1;
+    Console.WriteLine(text);
+    for (int i = 0; i < array.Length - 1; i += 2)
+    {
+        array[i] = InputNumber($"x{count}: ");
+        array[i + 1] = InputNumber($"y{count}: ");
+        count++;
+        //if(i==6) break;
+    }
+}
+void PrintCoord(double[] array)
+{
+    for (int i = 0; i < array.Length; i += 2) Console.Write($"({array[i]}, {array[i + 1]}) ");
+    Console.WriteLine();
+}
+
+Console.Clear();
+int quantity = (int)InputNumber("Введите количество точек в фигуре: ");
+double[] array = new double[quantity * 2];
+InputCoord(array, "Введите координаты точек: ");
+PrintCoord(array);
+
+double k = 0;
+double[] arr = { 2, 4, 0.5 };
+int i =0;
+bool stop = false;
+while (true)
+{
+    k = InputNumber("Введите коэффициент масштабирования (2; 4; 0,5): ");
+    for(i=0; i<arr.Length;i++)
+    {
+        if (arr[i] == k) stop = true;
+    }
+    if (stop == true) break;
+}
+
+double[] newArray = new double[array.Length];
+for (int j = 0; j < array.Length; j++) newArray[j] = array[j] * k;
+PrintCoord(newArray);
